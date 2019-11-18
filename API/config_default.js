9c3e470f8012f9ae870d3075cc54c2560abe2510
Ediblewildfungi <jianghhui@gmail.com>
2019-11-18 09:17:11 +0800

    mewtea maid

---
+++ API/config_default.js
@@ -0,0 +1,22 @@
+configs = {
+
+    // 服务信息
+    'server': {
+        "version": "v1.0", 
+    },
+
+    // 数据库信息
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'www-data',
+        'password': 'www-data',
+        'database': 'awesome'
+    },
+
+    // 
+    'session': {
+        'secret': 'AwEsOmE'
+    }
+
+}
