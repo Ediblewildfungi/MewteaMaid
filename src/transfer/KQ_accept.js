45c2817b5e2dd35fb865190e7334f622d41b717c
Ediblewildfungi <jianghhui@gmail.com>
2020-01-13 03:24:05 +0800

    feat: feat: update KQ bot js

--- src/transfer/KQ_accept.js
+++ src/transfer/KQ_accept.js
@@ -1,11 +1,23 @@
 // 服务入口 接收CQP 的上报消息 并转换为 API指令传入
 
-var http = require('http')
-var transfer = require('./transfer')
+const path = require('path')
+const http = require('http')
+const transfer = require('./transfer')
+const Logger = require('../helpers/logger')
+
+const logger = Logger(path.resolve(__dirname, '../../logs'), process.env.NODE_ENV !== 'development')
+
+//核心服务端口
+const PORT = process.env.PORT || 3600
+
+//酷Q应用服务端口
+const KQ_PORT = 5000
 
 class KQ_Accept {
   constructor() {
 
+    
+    
     // HTTP服务
     http.createServer(function (req, res) {
       var content = ""
@@ -26,9 +38,17 @@ class KQ_Accept {
         console.log(message_data.self_id)
         console.log()
 
-        // 信息传入
-        // 前端类型
-        // const transfer = new Transfer()
+        //格式化字符串，替换换行字符
+        message_data.message = message_data.message.replace(/[\r]/g," /r "); 
+        message_data.message = message_data.message.replace(/[\n]/g," /n "); 
+
+        //消息日志记录
+        logger.info(`KQ_Accept - Accept - ID:${ message_data.self_id }  message: ${ message_data.message }`)
+
+        //消息处理
+        // const transfer = new transfer.Transfer("KQ_Accept",message_data.user_id,message_data.message_type,message_data.group_id,message_data.message)
+
+
         
 
 
@@ -36,9 +56,10 @@ class KQ_Accept {
       })
 
 
-    }).listen(5000)
+    }).listen(KQ_PORT)
   }
 }
 
 
 const accept = new KQ_Accept()
+logger.info(`KQ_Accept is running on port ${ KQ_PORT }`)
