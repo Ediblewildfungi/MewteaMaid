85f2566fad24c08c8473f3c164bd995f7d617a29
Ediblewildfungi <jianghhui@gmail.com>
2020-01-16 22:18:33 +0800

    feat: update KQ_accept js

--- src/transfer/KQ_accept.js
+++ src/transfer/KQ_accept.js
@@ -10,14 +10,13 @@ const logger = Logger(path.resolve(__dirname, '../../logs'), process.env.NODE_EN
 //核心服务端口
 const PORT = process.env.PORT || 3600
 
-//酷Q应用服务端口
-const KQ_PORT = 5000
+const KQ_PORT = process.env.PORT || 5000
 
 class KQ_Accept {
   constructor() {
 
-    
-    
+
+
     // HTTP服务
     http.createServer(function (req, res) {
       var content = ""
@@ -39,17 +38,46 @@ class KQ_Accept {
         console.log()
 
         //格式化字符串，替换换行字符
-        message_data.message = message_data.message.replace(/[\r]/g," /r "); 
-        message_data.message = message_data.message.replace(/[\n]/g," /n "); 
+        message_data.message = message_data.message.replace(/[\r]/g, " /r ");
+        message_data.message = message_data.message.replace(/[\n]/g, " /n ");
 
         //消息日志记录
-        logger.info(`KQ_Accept - Accept - ID:${ message_data.self_id }  message: ${ message_data.message }`)
+        logger.info(`KQ_Accept - Accept - ID:${message_data.self_id}  message: ${message_data.message}`)
 
         //消息处理
-        // const transfer = new transfer.Transfer("KQ_Accept",message_data.user_id,message_data.message_type,message_data.group_id,message_data.message)
+        //群消息处理
+        if (message_data.message_type == "group") {
+
+          //消息传入语言处理单元
+          const transfer2 = new transfer("KQ_Accept", "1034614154", "message_data.user_id", "message_data.message_type", "message_data.group_id", message_data.message)
+
+          // const transfer2 = new transfer("KQ_Accept", "1034614154", "message_data.user_id", "message_data.message_type", "message_data.group_id", "喵一言")
+          transfer2.REmessage.then(function (value) {
+            if (value !== 0) {
+              //返回值操作
+              console.log(value)
+              const RESrc = "http://192.168.199.100:5700/send_group_msg?group_id=" + message_data.group_id + "&message=" + value
+              http.get(RESrc, function (req, res) {
+                var content = ''
+                req.on('data', function (data) {
+                  content += data
+                })
+                req.on('end', function () {
+                  // console.info(content)
+                  // var hitokoto_data = JSON.parse(content)
+                  console.log(content)
+                })
+              })
+            }
 
 
-        
+          })
+
+        } else {
+
+        }
+
+        //私聊消息处理
 
 
         res.end()
@@ -62,4 +90,4 @@ class KQ_Accept {
 
 
 const accept = new KQ_Accept()
-logger.info(`KQ_Accept is running on port ${ KQ_PORT }`)
+logger.info(`KQ_Accept is running on port ${KQ_PORT}`)
