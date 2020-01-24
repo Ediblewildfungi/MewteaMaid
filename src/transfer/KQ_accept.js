4ac7331cc7078da07ed3310152dfeb41e49e05f6
Ediblewildfungi <jianghhui@gmail.com>
2020-01-25 02:06:33 +0800

    feat: update send private msg

--- src/transfer/KQ_accept.js
+++ src/transfer/KQ_accept.js
@@ -10,7 +10,7 @@ const logger = Logger(path.resolve(__dirname, '../../logs'), process.env.NODE_EN
 //核心服务端口
 const PORT = process.env.PORT || 3600
 
-const KQ_PORT = process.env.PORT || 5000
+const KQ_PORT = 5000
 
 class KQ_Accept {
   constructor() {
@@ -85,35 +85,81 @@ class KQ_Accept {
                 res.on('data', function (data) {
                   content += data
                 })
-
                 res.on('end', function () {
 
                   //格式化返回参数
                   content = JSON.parse(content)
                   console.log(content)
                 })
-
                 req.on('error', function (err) {
                   // handle error.
                   console.log(err)
                 })
               })
-
               req.write(REbody)
               req.end()
-
             }
+          })
+        }
+        //私聊消息处理 
+        else if (message_data.message_type == "private") {
+          //消息传入语言处理单元
+          const getTransfer = new transfer("KQ_Accept", "1034614154", "message_data.user_id", message_data.message_type, "message_data.group_id", message_data.message)
 
+          // const transfer2 = new transfer("KQ_Accept", "1034614154", "message_data.user_id", "message_data.message_type", "message_data.group_id", "喵一言")
+          getTransfer.REmessage.then(function (value) {
+            if (value !== 0) {
 
+              //返回值操作
+
+              //返回信息内容
+              var REbody = {
+                "user_id": message_data.user_id,
+                "message": value
+              }
+
+              //格式化
+              REbody = JSON.stringify(REbody)
+
+              //http post参数
+              var REmessageOptions = {
+                host: '192.168.199.100',
+                port: 5700,
+                path: '/send_private_msg',
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                  'Content-Length': REbody.length
+                }
+              }
+              console.log(value)
+
+              //向CoolQ HTTP发送请求
+              var req = http.request(REmessageOptions, function (res) {
+                res.setEncoding('utf-8')
+                var content = ''
+                res.on('data', function (data) {
+                  content += data
+                })
+                res.on('end', function () {
+
+                  //格式化返回参数
+                  content = JSON.parse(content)
+                  console.log(content)
+                })
+                req.on('error', function (err) {
+                  // handle error.
+                  console.log(err)
+                })
+              })
+              req.write(REbody)
+              req.end()
+            }
           })
 
-        } else if (message_data.message_type == "private") {
-          
         } else {
-
+          console.log("消息内容未知")
         }
-
-        //私聊消息处理
 
 
         res.end()
