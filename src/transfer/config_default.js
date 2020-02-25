63b2b2add534137a2b5795d50a43e63109f8b8fe
Ediblewildfungi <jianghhui@gmail.com>
2020-02-26 03:29:03 +0800

    chore: add config template

---
+++ src/transfer/config_default.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+    database: {
+        host: "localhost",
+        user: "",
+        password: "",
+        database: "-"
+    },
+    key: {
+
+        //和风天气平台KEY
+        weatherKey: "",
+
+        //FFLOGS 公钥
+        fflogsKey: "",
+    }
+};
