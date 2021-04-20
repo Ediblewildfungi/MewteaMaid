9627959fbe2d7cc4478bd4103e9d3f3e2ad9c07b
Ediblewildfungi <jianghhui@gmail.com>
2021-04-21 02:15:42 +0800

    chore : config update & file sort out

--- src/transfer/config_default.js
+++ src/transfer/config_default.js
@@ -1,38 +1,41 @@
 module.exports = {
 
     Admin: {
-        id: 00000,
-        group: 00000
+        id: 000,
+        group: 000
     },
     server: {
 
         //运行妹抖的机器IP地址
-        host: "localhost",
+        host: "192.168.199.100",
 
         //服务端口
         port: 5700,
 
         //KQ服务端口
-        KQ_PORT: 5000,
+        KQ_PORT: 5200,
 
         //Mirai http服务端口
-        MI_PORT: 5200,
+        MI_PORT: 5700,
 
         //Mirai http AUTH_KEY
         MI_AUTHKEY: "MEWTEAMAID",
 
+        //Mirai http sessionKey
+        MI_SKey: "000",
+
         //SETU url
-        SETU: "http://192.168.199.100/d/setu/",
+        SETU: "http://127.0.0.1/setu/",
 
         //SETU_NUM
-        SETU_NUM: 13
+        SETU_NUM: 29
 
     },
     database: {
         host: "localhost",
         user: "",
         password: "",
-        database: "000"
+        database: "b1imd"
     },
     key: {
 
